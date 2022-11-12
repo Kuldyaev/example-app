dc03edf6847e421b66e0e232b6ccf42d39480f66
Kuldyaev <slavalion1@gmail.com>
2022-11-13 01:52:28 +0300

    main layout was added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,15 @@
-<php
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Домашняя</title>
-        </head>
-        <body class="firstpage">
-            <h2><i>Приветствуем Вас на домашней странице нашего агрегатора новостей</i></h2>
-            <br>
-@include('menu')
-            <h3>Данный проект разрабатывается в рамках курса Laravel от GeekBrains</h3>
-        </body>
-    </html>
+@extends('layouts.main')
+
+@section('title')
+@parent | Главная
+@endsection
+
+@section('menu')
+    @include('menu')
+@endsection
+
+@section('content')
+    <h2><i>Приветствуем Вас на домашней странице нашего агрегатора новостей</i></h2>
+    <br>
+    <h3>Данный проект разрабатывается в рамках курса Laravel от GeekBrains</h3>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@section('title')Страница @show</title>
+    </head>
+    <body class="antialiased">
+@yield('menu')
+@yield('content')
+    </body>
+</html>
