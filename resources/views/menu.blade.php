b18841863edca033cc6c2bdfd1ca5f9906df0203
Kuldyaev <slavalion1@gmail.com>
2022-11-15 18:44:54 +0300

    Finaly bootstrap was realy added

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,5 +1,5 @@
-<a href="{{ route('home') }}" class="link-danger">Главная</a>
-<a href="{{ route('news.categories.all') }}" class="link-primary">Категории новостей</a>
-<a href="{{ route('news.showAllNews') }}" class="link-primary">Все новости</a>
-<a href="{{ route('news.addOne') }}" class="link-primary">Добавить новость</a>
-<a href="{{ route('auth') }}" class="nav-link">Авторизация</a>
+<li><a href="{{ route('home') }}" class="link-danger">Главная</a></li>
+<li><a href="{{ route('news.categories.all') }}" class="link-primary">Категории новостей</a></li>
+<li><a href="{{ route('news.showAllNews') }}" class="link-primary">Все новости</a></li>
+<li><a href="{{ route('news.addOne') }}" class="link-primary">Добавить новость</a></li>
+<li><a href="{{ route('auth') }}" class="nav-link">Авторизация</a></li>
