1c377862082af0ac00df098ffff23c9f1d3e6e4c
Kuldyaev <slavalion1@gmail.com>
2022-11-16 22:31:15 +0300

    Bootstrap added to One news page

--- resources/views/news/showOne.blade.php
+++ resources/views/news/showOne.blade.php
@@ -5,18 +5,27 @@
 @endsection
 
 @section('menu')
-    @include('menu')
+@include('menu')
 @endsection
 
 @section('content')
-    <br>
-    @if ($news)
-        <h2>{{ $news['title'] }}</h2>
-        <h3><i>{{ $news['shortDescription'] }}</i></h3>
-        <br>
-        <p>{{ $news['textInfo'] }}</p>
-    @else
-        <br>
-        <p> нет такой новости </p>
-    @endif
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        @if ($news)
+                            <h2>{{ $news['title'] }}</h2>
+                            <h3><i>{{ $news['shortDescription'] }}</i></h3>
+                            <br>
+                            <p>{{ $news['textInfo'] }}</p>
+                        @else
+                            <br>
+                            <p> нет такой новости </p>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
