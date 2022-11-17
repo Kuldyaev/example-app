7bec79b660f4d9279ffefb3e471e3b7fed4f11ae
Kuldyaev <slavalion1@gmail.com>
2022-11-17 13:47:07 +0300

    views for admin area finished

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,4 +1,15 @@
-<div class="container">
+@extends('layouts.admin')
+
+@section('title')
+@parent | Админка
+@endsection
+
+@section('menu')
+@include('admin.menu')
+@endsection
+
+@section('content')
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -18,3 +29,4 @@
             </div>
         </div>
     </div>
+@endsection
--- resources/views/admin/test1.blade.php
+++ resources/views/admin/test1.blade.php
@@ -1,21 +1,23 @@
-<div class="container">
+@extends('layouts.admin')
+
+@section('title')
+@parent | Админка
+@endsection
+
+@section('menu')
+@include('admin.menu')
+@endsection
+
+@section('content')
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-body">
                         <h4>TEST1</h4>
-                        <br>
-                        <form>
-                            <label for="login">Логин:</label><br>
-                            <input type="text" id="login" name="login"><br>
-                            <label for="pass">Пароль:</label><br>
-                            <input type="text" id="pass" name="pass"><br>
-                            <input type="radio" id="remember" name="remember" value="запомнить">
-                            <label for="remember">запомнить</label><br>
-                            <input type="submit" value="Submit">
-                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
